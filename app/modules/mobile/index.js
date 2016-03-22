f3212b2167c63b0397b37abfef65dfb6a52ffa9e
gordiienko <lozuvatka@gmail.com>
2016-03-22 17:00:43 +0200

    Add mobile page

--- app/modules/mobile/index.js
+++ app/modules/mobile/index.js
@@ -1,15 +1,14 @@
 require('./index.sass');
 
-angular.module('main', [
+angular.module('mobile', [
 	require('_modules/payments/card2card'),
 	require('angular-ui-bootstrap/src/accordion')
 ])
 	.config(['$stateProvider',
 		function($stateProvider) {
 			$stateProvider
-				.state('app.main', {
-					url: '/',
-					controller: require('./controllers/main'),
+				.state('app.mobile', {
+					url: '/mobile',
 					template: require('./templates/main.html'),
 					data: {
                         access: {}
@@ -18,4 +17,4 @@ angular.module('main', [
 		}
 	]);
 
-module.exports = 'main';
+module.exports = 'mobile';
