e946c70d1a116beee956ff41b9aaca07338c2a10
Alexander Midtsev <midtsev@gmail.com>
2016-04-14 17:08:54 +0300

    check functions

--- app/pages/send/pages/card/controllers/main.js
+++ app/pages/send/pages/card/controllers/main.js
@@ -1,5 +1,35 @@
 Ctrl.$inject = ['$scope'];
 function Ctrl($scope) {
+    $scope.checkNumber = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 16) {
+            return true;
+        }
+    };
+    $scope.checkMonth = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 2) {
+            return true;
+        }
+    };
+    $scope.checkYear = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 2) {
+            return true;
+        }
+    };
+    $scope.checkCvc = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 3) {
+            return true;
+        }
+    };
+    $scope.checkPhone = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 9) {
+            return true;
+        }
+    };
+    $scope.checkTargetNumber = function (ctrl) {
+        if (ctrl.$valid && ctrl.$modelValue && ctrl.$modelValue.length == 16) {
+            return true;
+        }
+    };
 }
 
 module.exports = Ctrl;
